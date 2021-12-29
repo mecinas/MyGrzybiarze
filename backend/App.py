206be83fa92825fd20d47ea10d37238791c0dd1f
mecinas <szymon.mecina@vp.pl>
2021-12-29 18:20:56 +0100

    Adding backend Azure service

--- backend/App.py
+++ backend/App.py
@@ -3,11 +3,13 @@ from flask import Flask, request, make_response, send_file
 from RESTControllers.AccountModel import user_controller
 from RESTControllers.FriendshipModel import friendship_controller
 from RESTControllers.NotificationModel import notification_controller
+from RESTControllers.AzureModel import azure_controller
 
 app = Flask(__name__)
 app.register_blueprint(user_controller)
 app.register_blueprint(friendship_controller)
 app.register_blueprint(notification_controller)
+app.register_blueprint(azure_controller)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
---
+++ backend/RESTControllers/AzureModel.py
@@ -0,0 +1,63 @@
+from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
+from msrest.authentication import ApiKeyCredentials
+import os, time, uuid, json
+from dotenv import load_dotenv
+from flask import Flask, request, make_response, send_file, Blueprint, jsonify
+
+from RESTControllers.ConfigREST import allowed_methods, send_response
+
+load_dotenv()
+ENDPOINT = os.getenv('ENDPOINT')
+PREDICTION_KEY = os.getenv('PREDICTION_KEY')
+PREDICTION_RESOURCE_ID = os.getenv('PREDICTION_RESOURCE_ID')
+PREDICTION_EDIBLE_PROJECT_ID = os.getenv('PREDICTION_EDIBLE_PROJECT_ID')
+PUBLISH_EDIBLE_ITERATION_NAME = os.getenv('PUBLISH_EDIBLE_ITERATION_NAME')
+PREDICTION_SPECIES_PROJECT_ID = os.getenv('PREDICTION_SPECIES_PROJECT_ID')
+PUBLISH_SPECIES_ITERATION_NAME = os.getenv('PUBLISH_SPECIES_ITERATION_NAME')
+
+prediction_credentials = ApiKeyCredentials(
+    in_headers={"Prediction-key": PREDICTION_KEY})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+
+azure_controller = Blueprint('AzureRESTController', __name__)
+
+
+@azure_controller.route('/analyse/edible', methods=['POST', 'OPTIONS'])
+def alyse_edible():
+    if(request.method == 'OPTIONS'):
+        return allowed_methods(['POST'])
+
+    image = request.files["mushroom_image"]
+    results = predictor.classify_image(
+            PREDICTION_EDIBLE_PROJECT_ID, PUBLISH_EDIBLE_ITERATION_NAME, image.read())
+
+    array_of_predictions = []
+    for prediction in results.predictions:
+        prediction_dto = {
+            "tag_name": prediction.tag_name,
+            "probability": "{0:.2f}%".format(prediction.probability * 100)
+        }
+        array_of_predictions.append(prediction_dto)
+
+    return send_response(jsonify(array_of_predictions))
+
+@azure_controller.route('/analyse/species', methods=['POST', 'OPTIONS'])
+def alyse_species():
+    if(request.method == 'OPTIONS'):
+        return allowed_methods(['POST'])
+
+    image = request.files["mushroom_image"]
+    results = predictor.classify_image(
+            PREDICTION_SPECIES_PROJECT_ID, PUBLISH_SPECIES_ITERATION_NAME, image.read())
+
+    array_of_predictions = []
+    for prediction in results.predictions:
+        prediction_dto = {
+            "tag_name": prediction.tag_name,
+            "probability": "{0:.2f}%".format(prediction.probability * 100)
+        }
+        array_of_predictions.append(prediction_dto)
+
+    return send_response(jsonify(array_of_predictions))
